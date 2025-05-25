8b3b4f81da8c3c5571eca414f5d272a354f19284
stefanoeagledev <stefano.work@hotmail.com>
2025-05-25 15:26:08 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { SearchComponent } from './search/search.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, SearchComponent],
+  imports: [RouterOutlet, HeaderComponent],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,17 @@
 import { Routes } from '@angular/router';
-import { ResultsComponent } from './results/results.component'; // adjust path if needed
+import { HomeComponent } from './home/home.component';
+import { ResultsComponent } from './results/results.component';
 
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./app.component').then((m) => m.AppComponent),
+    loadComponent: () =>
+      import('./home/home.component').then((m) => m.HomeComponent),
   },
   {
     path: 'venda/imoveis/:estado/:cidade',
     loadComponent: () =>
       import('./results/results.component').then((m) => m.ResultsComponent),
   },
-  // Optional: add a wildcard to handle not found
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
