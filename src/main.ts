e7e54180d1e55db6474df359bc8fb91902763cc4
stefanoeagledev <stefano.work@hotmail.com>
2025-05-25 15:13:33 -0300

    commmit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app/app.routes';
-
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
-});
+  providers: [provideRouter(routes), provideHttpClient()],
+}).catch((err) => console.error(err));
