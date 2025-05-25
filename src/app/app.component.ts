95755203fe39490d70ad3ef801f204a6e9cb19a8
stefanoeagledev <stefano.work@hotmail.com>
2025-05-25 14:23:33 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, SearchComponent],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,36 @@
+import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css'],
+})
+export class SearchComponent {
+  states = [
+    {
+      code: 'SP',
+      name: 'São Paulo',
+      cities: ['São Paulo', 'Campinas', 'Santos'],
+    },
+    {
+      code: 'RJ',
+      name: 'Rio de Janeiro',
+      cities: ['Rio de Janeiro', 'Niterói', 'Petrópolis'],
+    },
+    // Add more states/cities as needed
+  ];
+
+  selectedState = this.states[0];
+  selectedCity = this.states[0].cities[0];
+
+  onStateChange(event: any) {
+    const state = this.states.find((s) => s.code === event.target.value);
+    if (state) {
+      this.selectedState = state;
+      this.selectedCity = state.cities[0];
+    }
+  }
+}
